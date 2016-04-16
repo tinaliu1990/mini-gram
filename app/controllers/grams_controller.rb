a07df144f05a2538ee3986492bc5962e1b3536e5
starter2014 <tinaliuliang@gmail.com>
2016-04-16 08:36:58 +0000

    minigrams

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,35 @@
+class GramsController < ApplicationController
+    def index
+        @grams = Gram.all
+    end
+    
+    def new
+        @gram = Gram.new
+    end
+    
+    def create
+        @gram = Gram.new(model_params)
+        if @gram.save
+            redirect_to @gram
+        else
+            render :new
+        end
+    end
+    
+    def show
+        @gram = Gram.find(params[:id])
+    end
+    
+    def destroy
+        @gram = Gram.find(params[:id])
+        @gram.destroy
+        
+        redirect_to grams_path
+    end
+    
+    private
+    
+    def model_params
+        params.require(:gram).permit(:title, :description, :location_code)
+    end
+end
