dce59074b398f02a73cd5ff07459d2237582048a
starter2014 <tinaliuliang@gmail.com>
2016-04-20 13:24:52 +0000

    add country_select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def country_name(country_code)
+        country = ISO3166::Country[country_code]
+        country.translations[I18n.locale.to_s]
+        #country.translations['zh']
+    end
 end
